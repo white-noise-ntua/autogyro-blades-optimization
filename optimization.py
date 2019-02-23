15bec7f68ff95cad75f5912cfb9da8dafe94a69b
Miltiadis Stouras <m.stouras@gmail.com>
2019-02-23 17:04:54 +0200

    Add a function to return the propeller's array (radius,chord,twist). Change functions to take propeller's length L as a parameter

--- optimization.py
+++ optimization.py
@@ -1,6 +1,5 @@
 from subprocess import PIPE, Popen
 
-L = 0.257 # length of helix
 
 def runSimulation():
     command = "timeout 10s wine aero_static.exe || [ $? -eq 124 ] && echo TIMEOUT_ERROR"
@@ -53,15 +52,36 @@ def computeI(r,c):
     return I
 
 
-def chordBezier(y1,y2,y3,y4,t):
+def chordBezier(y1,y2,y3,y4,t,L):
     c = (1-t)**3 * y1 + 3 * (1-t)**2 * t * y2 + 3 * (1-t) * t**2 * y3 + t**3 * y4
     r = (t**3 + (1-t)**2 * t + 2*(1-t) * t**2) * L
 
     return c,r
 
-def twistBezier(f1,f2,f3,r):
+def twistBezier(f1,f2,f3,r,L):
     t = r/L
 
     f = (1-t)**2 * f1 + 2*(1-t)*t*f2 + t**2 * f3
 
     return f
+
+def getPropellerArray(y1,y2,y3,y4,f1,f2,f3,L):
+    radius = []
+    chord = []
+    twist = []
+
+    for i in range(10):
+        t = 1/9 * i
+        c,r = chordBezier(y1,y2,y3,y4,t,L)
+        radius.append(r)
+        chord.append(c)
+        twist.append(twistBezier(f1,f2,f3,r,L))
+
+    return radius,chord,twist
+
+
+if __name__ == '__main__':
+    L = 0.257 # propeller's length
+    r,c,t = getPropellerArray(0.05,0.045,0.035,0.02,25,25,25,L)
+    writeInput(r,c,t)
+    print(runSimulation())
