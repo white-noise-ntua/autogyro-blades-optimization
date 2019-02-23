2dd2309fcf43ad5a50993f38715f98a06593f395
Miltiadis Stouras <m.stouras@gmail.com>
2019-02-23 13:35:55 +0200

    Add functions to write the desired simulation input to geomb.inp file

--- optimization.py
+++ optimization.py
@@ -1,11 +1,5 @@
 from subprocess import PIPE, Popen
 
-f = open("geomb.inp","r")
-lines = [ _ for _ in f ]
-#data = [list(map(float,x.split())) for x in lines[3:-1]]
-intro = lines[:3]
-
-
 def runSimulation():
     command = "timeout 10s wine aero_static.exe || [ $? -eq 124 ] && echo TIMEOUT_ERROR"
     process = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
@@ -18,6 +12,25 @@ def runSimulation():
     terminalAngularVelocity = float(str(out.decode("utf-8")).split('\n')[-4].split()[5])
 
     return terminalAngularVelocity
+
+
+def inputFileIntro(numberOfPoints):
+    result = "-- Input geometry blade\n"
+    result += str(numberOfPoints)
+    result += "    ! NSPANB\n"
+    result += "  r     chord    twist    xaer    zaer\n"
+
+    return result
+
+
+def writeInput(r,chord,twist):
+    f = open("geomb.inp","w")
+
+    f.write(inputFileIntro(len(r)))
+
+    for i in range(len(r)):
+        f.write(str(r[i]) + '\t' + str(chord[i]) + '\t' + str(twist[i]) + '\t0.00\t0.00\n')
+
 
 if __name__ == '__main__':
     runSimulation()
