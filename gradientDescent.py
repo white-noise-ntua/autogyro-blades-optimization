36c4c4404458a1be8d6f2fc62e8da5aa4a943ebd
Miltiadis Stouras <m.stouras@gmail.com>
2019-03-14 20:14:59 +0200

    Add routines to compute the remaining partial derivatives

--- gradientDescent.py
+++ gradientDescent.py
@@ -9,6 +9,7 @@ L = 0.257 # propeller's length
 
 def gradientStep(y1,y2,y3,y4,t1,t2,t3,currentCost):
 
+    # -------------------------------------------------------
     # Computing cost's partial derivative with respect to y1
 
     #calculate cost at y1+chordEpsilon
@@ -18,10 +19,69 @@ def gradientStep(y1,y2,y3,y4,t1,t2,t3,currentCost):
     newCost = calculateCost(w,v,r,c)
 
     grad_y1 =  (newCost - currentCost) / chordEpsilon
+    # -------------------------------------------------------
 
-    print(currentCost)
-    print(newCost)
-    print(grad_y1)
+
+    # -------------------------------------------------------
+    # Computing cost's partial derivative with respect to y2
+    r, c, t = getPropellerArray(y1,y2+chordEpsilon,y3,y4,t1,t2,t3,L)
+    writeInput(r,c,t)
+    w, v = runSimulation()
+    newCost = calculateCost(w,v,r,c)
+
+    grad_y2 =  (newCost - currentCost) / chordEpsilon
+    # -------------------------------------------------------
+
+    # -------------------------------------------------------
+    # Computing cost's partial derivative with respect to y3
+    r, c, t = getPropellerArray(y1,y2,y3+chordEpsilon,y4,t1,t2,t3,L)
+    writeInput(r,c,t)
+    w, v = runSimulation()
+    newCost = calculateCost(w,v,r,c)
+
+    grad_y3 =  (newCost - currentCost) / chordEpsilon
+    # -------------------------------------------------------
+
+    # -------------------------------------------------------
+    # Computing cost's partial derivative with respect to y4
+    r, c, t = getPropellerArray(y1,y2,y3,y4+chordEpsilon,t1,t2,t3,L)
+    writeInput(r,c,t)
+    w, v = runSimulation()
+    newCost = calculateCost(w,v,r,c)
+
+    grad_y4 =  (newCost - currentCost) / chordEpsilon
+    # -------------------------------------------------------
+
+    # -------------------------------------------------------
+    # Computing cost's partial derivative with respect to t1
+    r, c, t = getPropellerArray(y1,y2,y3,y4,t1+twistEpsilon,t2,t3,L)
+    writeInput(r,c,t)
+    w, v = runSimulation()
+    newCost = calculateCost(w,v,r,c)
+
+    grad_t1 =  (newCost - currentCost) / twistEpsilon
+    # -------------------------------------------------------
+
+    # -------------------------------------------------------
+    # Computing cost's partial derivative with respect to t2
+    r, c, t = getPropellerArray(y1,y2,y3,y4,t1,t2+twistEpsilon,t3,L)
+    writeInput(r,c,t)
+    w, v = runSimulation()
+    newCost = calculateCost(w,v,r,c)
+
+    grad_t2 =  (newCost - currentCost) / twistEpsilon
+    # -------------------------------------------------------
+
+    # -------------------------------------------------------
+    # Computing cost's partial derivative with respect to t3
+    r, c, t = getPropellerArray(y1,y2,y3,y4,t1,t2,t3+twistEpsilon,L)
+    writeInput(r,c,t)
+    w, v = runSimulation()
+    newCost = calculateCost(w,v,r,c)
+
+    grad_t3 =  (newCost - currentCost) / twistEpsilon
+    # -------------------------------------------------------
+
 
 if __name__ == '__main__':
     r,c,t = getPropellerArray(0.5,0.45,0.35,0.25,0,0,0,L)
